53e725cb0de7fab3f4ee3fd86100d784849bac9e
Ulyana <malyarchyk.17@gmail.com>
2019-11-18 16:37:53 +0200

    add about info

--- public/js/app.js
+++ public/js/app.js
@@ -2,32 +2,26 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-const host = 'localhost';
-// git push heroku master
 
-function main() {
-    weatherForm.addEventListener('submit', async (event) => {
-        event.preventDefault()
+weatherForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
 
-        messageOne.textContent = 'Loading...'
-        messageTwo.textContent = '';
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = '';
 
-        let url = '/weather?address=' + search.value
+    let url = '/weather?address=' + search.value
 
-        try {
-            let responce = await fetch(url)
-            let data = await responce.json()
+    try {
+        let responce = await fetch(url)
+        let data = await responce.json()
 
-            if (data.forecast === undefined) {
-                messageOne.textContent = data.message
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-        } catch (err) {
-            console.log(err)
+        if (data.forecast === undefined) {
+            messageOne.textContent = data.message
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
-    })
-}
-
-main()
+    } catch (err) {
+        console.log(err)
+    }
+})
